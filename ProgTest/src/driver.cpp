356d3d2139b862798e3b1254289ac47b92387010
Mark Fontenot <mfonten@lyle.smu.edU>
2013-09-11 05:47:37 -0500

    A First Commit

---
+++ ProgTest/src/driver.cpp
@@ -0,0 +1,15 @@
+/*
+ * driver.cpp
+ *
+ *  Created on: Sep 11, 2013
+ *      Author: mark
+ */
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	cout << "Hello World" << endl;
+	return 0;
+}

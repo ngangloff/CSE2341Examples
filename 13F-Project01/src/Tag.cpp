2b426e44c7d01437e3b8efe63832c17a43c8593c
Mark Fontenot <mfonten@lyle.smu.edU>
2013-09-14 16:33:21 -0500

    jjjjj

---
+++ 13F-Project01/src/Tag.cpp
@@ -0,0 +1,20 @@
+/*
+ * Tag.cpp
+ *
+ *  Created on: Sep 14, 2013
+ *      Author: mark
+ */
+
+#include "Tag.h"
+
+Tag::Tag()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+Tag::~Tag()
+{
+	// TODO Auto-generated destructor stub
+}
+
---
+++ 13F-Project01/src/Tag.h
@@ -0,0 +1,27 @@
+/*
+ * Tag.h
+ *
+ *  Created on: Sep 14, 2013
+ *      Author: mark
+ */
+
+#include "TaggedLocations.h"
+
+#ifndef TAG_H_
+#define TAG_H_
+
+class Tag
+{
+public:
+	Tag();
+	virtual ~Tag();
+
+
+private:
+	int tagger;
+	int tagged;
+	int timeStamp;
+	TaggedLocation location;
+};
+
+#endif /* TAG_H_ */

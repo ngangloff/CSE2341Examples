b54bdb0adac10b044cba950057695acbb8e9d6cc
Mark Fontenot <mfonten@lyle.smu.edU>
2013-09-14 16:22:37 -0500

    Added TaggedLocation enum and Tag Class

---
+++ 13F-Project01/src/TaggedLocations.h
@@ -0,0 +1,14 @@
+/*
+ * TaggedLocations.h
+ *
+ *  Created on: Sep 14, 2013
+ *      Author: mark
+ */
+
+#ifndef TAGGEDLOCATIONS_H_
+#define TAGGEDLOCATIONS_H_
+
+enum TaggedLocation{back, chest, shoulder, laserGun};
+
+
+#endif /* TAGGEDLOCATIONS_H_ */

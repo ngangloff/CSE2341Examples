8af91fdd8055a15854638acb292fea77362b9ada
Mark Fontenot <mfonten@lyle.smu.edU>
2013-09-14 16:46:39 -0500

    Added Match class to project

---
+++ 13F-Project01/src/Match.cpp
@@ -0,0 +1,20 @@
+/*
+ * Match.cpp
+ *
+ *  Created on: Sep 14, 2013
+ *      Author: mark
+ */
+
+#include "Match.h"
+
+Match::Match()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+Match::~Match()
+{
+	// TODO Auto-generated destructor stub
+}
+
---
+++ 13F-Project01/src/Match.h
@@ -0,0 +1,18 @@
+/*
+ * Match.h
+ *
+ *  Created on: Sep 14, 2013
+ *      Author: mark
+ */
+
+#ifndef MATCH_H_
+#define MATCH_H_
+
+class Match
+{
+public:
+	Match();
+	virtual ~Match();
+};
+
+#endif /* MATCH_H_ */
